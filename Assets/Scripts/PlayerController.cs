d6021ecf42cd1c9c8be71580e496e089161ffa70
Avery P <159206600+AveryPilcer@users.noreply.github.com>
2025-03-14 15:03:46 +1100

    added more collectibles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,7 +33,7 @@ public class PlayerController : MonoBehaviour
 
     void SetCountText() {
         countText.text = "Count:" + count.ToString();
-        if(count >= 12) {
+        if(count >= 14) {
             winTextObject.SetActive(true);
         }
     }
